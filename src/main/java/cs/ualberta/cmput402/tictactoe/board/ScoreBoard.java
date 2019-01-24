c3022772954bcc28cbcfd9bad6d66f43452017db
hiufungk <hiufungk@ualberta.ca>
2019-01-23 21:25:49 -0700

    added comments for ScoreBoard.java

--- src/main/java/cs/ualberta/cmput402/tictactoe/board/ScoreBoard.java
+++ src/main/java/cs/ualberta/cmput402/tictactoe/board/ScoreBoard.java
@@ -1,5 +1,8 @@
 package cs.ualberta.cmput402.tictactoe.board;
 
+/**
+ * ScoreBoard class which displays the amount of wins losses and ties
+ */
 public class ScoreBoard {
 	private int xWin;
 	private int oWin;
@@ -11,18 +14,22 @@ public class ScoreBoard {
 		tie = 0;
 	}
 	
+	//setter for score of X when X wins
 	public void setXWin() {
 		xWin++;
 	}
 	
+	//setter for score of O when O wins
 	public void setOWin() {
 		oWin++;
 	}
 	
+	//setter for tie
 	public void setTie() {
 		tie++;
 	}
 	
+	//display the score board
 	public void printScoreBoard() {
 		System.out.println("*****************************************************");
 		System.out.printf("Player X won: %d games, lost %d games\n",xWin, oWin);
